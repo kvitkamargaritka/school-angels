6fab959585835d7b38250e8fa305a72998bf77d6
Margarita <rita.akhmadulina@gmail.com>
2016-05-25 20:53:40 +0300

    add new sections

--- build/js/app.js
+++ build/js/app.js
@@ -45,6 +45,7 @@ $(document).ready(function() {
   var swiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
+    speed: 1000,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev'
   });
--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var swiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
+    speed: 1000,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev'
   });
