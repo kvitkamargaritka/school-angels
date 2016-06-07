d5e55ac603575e989d81b8f0dc4a4d446bf2a472
Margarita <rita.akhmadulina@gmail.com>
2016-06-07 11:25:50 +0300

    add custom elements

--- build/js/app.js
+++ build/js/app.js
@@ -55,6 +55,27 @@ $(document).ready(function() {
   $('#nav-icon').click(function(){
 		$(this).toggleClass('open');
     $('#nav-mob').fadeToggle();
-	});
+	 });
+
+  // inputs
+  var i = $('.form-group__input');
+  i.on('focusout', function() {
+    if ($(this).val() != '') {
+      $(this).addClass('input-filled');
+      //console.log('input не пустой');
+    }
+    else {
+      $(this).removeClass('input-filled');
+      //console.log('input пустой');
+    }
+  });
+
+  i.on('focus', function() {
+    $(this).parent().parent().find('.field__line').addClass('width-6');
+  });
+
+  i.on('focusout', function() {
+    $(this).parent().parent().find('.field__line').removeClass('width-6');
+  });
 
 });
